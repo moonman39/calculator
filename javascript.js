bb934395c1be51d54946c9a9abb182ada056c08c
Grant Luna <grant.luna@fnf.com>
2022-06-13 16:40:41 -0700

    Create operate function which takes in an operator & two numbers, and calls one of the original operation functions declared via add, subtract, multiply, and divide.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// Functions for add, subtract, multiple, and divide
 const add = (num1, num2) => {
   return num1 + num2;
 };
@@ -12,4 +13,22 @@ const multiply = (num1, num2) => {
 
 const divide = (num1, num2) => {
   return num1 / num2;
+};
+
+// Operate Function
+const operate = (operator, num1, num2) => {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+      break;
+    case "-":
+      return subtract(num1, num2);
+      break;
+    case "*":
+      return multiply(num1, num2);
+      break;
+    case "divide":
+      return divide(num1, num2);
+      break;
+  }
 };
