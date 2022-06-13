4aa5b5c725dae82f9550095b63f7e1aee4e4eeaa
Grant Luna <grant.luna@fnf.com>
2022-06-13 16:33:17 -0700

    Create add, subtract, multiply, and divide function which do as the name states.

---
+++ javascript.js
@@ -0,0 +1,15 @@
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+
+const subtract = (num1, num2) => {
+  return num1 - num2;
+};
+
+const multiply = (num1, num2) => {
+  return num1 * num2;
+};
+
+const divide = (num1, num2) => {
+  return num1 / num2;
+};
