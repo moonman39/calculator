76d4c0f947794b566db5f8c300ce6e76e0ade544
Grant Luna <grant.luna@fnf.com>
2022-06-15 14:44:26 -0700

    Edit all the functions.

--- javascript.js
+++ javascript.js
@@ -24,36 +24,72 @@ const operate = (operator, num1, num2) => {
     case "-":
       return subtract(num1, num2);
       break;
-    case "*":
+    case "×":
       return multiply(num1, num2);
       break;
-    case "divide":
+    case "÷":
       return divide(num1, num2);
       break;
   }
 };
 
-// Function to populate the display when number buttons are clicked
+// Change the display to the input
 const display = document.querySelector(".display-text");
-let displayValue;
+let displayValue = [];
+const inputs = document.querySelectorAll(".input-box");
+let operatorClicked;
+let operation;
 
-const inputButtons = document.querySelectorAll(".input-box");
-inputButtons.forEach((button) =>
-  button.addEventListener("click", () => {
+inputs.forEach((input) => {
+  input.addEventListener("click", () => {
     if (display.textContent === "0") {
-      display.textContent = button.textContent;
-      displayValue = button.textContent;
-      console.log(displayValue);
+      display.textContent = input.textContent;
+    } else if (operatorClicked === true) {
+      display.textContent = input.textContent;
+      operatorClicked = false;
     } else {
-      display.textContent += button.textContent;
-      displayValue = displayValue + button.textContent;
+      display.textContent += input.textContent;
+    }
+  });
+});
+
+// Operator event listener
+const operators = document.querySelectorAll(".operator");
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    operatorClicked = true;
+    displayValue.push(Number(display.textContent));
+    console.log(displayValue);
+    if (displayValue.length === 2) {
+      display.textContent = operate(
+        operation,
+        displayValue[0],
+        displayValue[1]
+      );
+      displayValue = [Number(display.textContent)];
       console.log(displayValue);
     }
-  })
-);
+    operation = operator.textContent;
+    console.log(operation);
+  });
+});
 
-// Function to clear the display when clicked
+// Equals event listener
+const equals = document.querySelector(".equals");
+equals.addEventListener("click", () => {
+  displayValue.push(Number(display.textContent));
+  console.log(displayValue);
+  display.textContent = operate(operation, displayValue[0], displayValue[1]);
+  displayValue = [];
+});
+
+// Clear the display
 const clear = document.querySelector(".clear");
 clear.addEventListener("click", () => {
-  display.textContent = 0;
+  display.textContent = "0";
+  displayValue = [];
 });
+
+// // Eventually, I think I can change the mechanics of toggling the operator
+// clicked variable to something like toggling a class.  Could even change the back-
+// ground color to stay highlighted, e.g.
