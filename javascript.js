4acd568b38b716f4434fdeefcf270aab85eb80ce
Grant Luna <grant.luna@fnf.com>
2022-06-15 21:01:23 -0700

    Start from scratch.  Code became too complicated.  Want to make it cleaner.

--- javascript.js
+++ javascript.js
@@ -33,68 +33,6 @@ const operate = (operator, num1, num2) => {
   }
 };
 
-// Change the display to the input
-const display = document.querySelector(".display-text");
-let displayValue = [];
-const inputs = document.querySelectorAll(".input-box");
-let operatorClicked;
-let operation;
-
-inputs.forEach((input) => {
-  input.addEventListener("click", () => {
-    if (display.textContent === "0") {
-      display.textContent = input.textContent;
-    } else if (operatorClicked === true) {
-      display.textContent = input.textContent;
-      operatorClicked = false;
-    } else {
-      display.textContent += input.textContent;
-    }
-  });
-});
-
-// Operator event listener
-const operators = document.querySelectorAll(".operator");
-operators.forEach((operator) => {
-  operator.addEventListener("click", () => {
-    operatorClicked = true;
-    displayValue.push(Number(display.textContent));
-    console.log(displayValue);
-    if (displayValue.length === 2) {
-      display.textContent = operate(
-        operation,
-        displayValue[0],
-        displayValue[1]
-      );
-      displayValue = [Number(display.textContent)];
-      console.log(displayValue);
-    }
-    operation = operator.textContent;
-    console.log(operation);
-  });
-});
-
-// Equals event listener
-const equals = document.querySelector(".equals");
-equals.addEventListener("click", () => {
-  if (displayValue.length === 0) {
-    display.textContent = display.textContent;
-    console.log(display.textContent);
-  } else {
-    displayValue.push(Number(display.textContent));
-    console.log(displayValue);
-    display.textContent = operate(operation, displayValue[0], displayValue[1]);
-    displayValue = [];
-  }
-});
-
-// Clear the display
-const clear = document.querySelector(".clear");
-clear.addEventListener("click", () => {
-  display.textContent = "0";
-  displayValue = [];
-});
-
 // // Eventually, I think I can change the mechanics of toggling the operator
 // clicked variable to something like toggling a class.  Could even change the back-
 // ground color to stay highlighted, e.g.
